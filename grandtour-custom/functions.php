3357f527a329a6120752518b3bfc9c2033412c76
Simon Archer <simon@archybold.com>
2018-09-06 12:11:51 +0100

    Added a script file to the theme.

--- grandtour-custom/functions.php
+++ grandtour-custom/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function grandtour_custom_enqueue_front_page_scripts()  {
+    // Enqueue the JS
+    wp_enqueue_script('grandtour-custom-all', get_stylesheet_directory_uri() . '/script.js', false, "", true);
+}
+add_action( 'wp_enqueue_scripts', 'grandtour_custom_enqueue_front_page_scripts' );
+
 function grandtour_deposits_form_output($productID) {
     $product = get_post($productID);
     if ( !is_null($product) && WC_Deposits_Product_Manager::deposits_enabled( $product->ID ) ) {
